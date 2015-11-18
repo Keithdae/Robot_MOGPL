c88a0342c308b21283ce9d30986c27ad7adafde3
Keithdae <keithdae@gmail.com>
2015-11-18 12:04:55 +0100

    Ajout du necessaire pour le programme

--- graph.h
+++ graph.h
@@ -9,6 +9,7 @@
 #include <string.h>
 
 
+
 class graph
 {
 	public:
@@ -17,8 +18,33 @@ class graph
 		
 
 		bool openFile(std::string fName);
-		bool closeFile();
+		bool closeFiles();
+
+		
 	private:
+		int xStart;
+		int yStart;
+		int xGoal;
+		int yGoal;
+
+		int dirStart;
+		/* Nord  => 0
+		*  Est   => 1
+		*  Sud   => 2
+		*  Ouest => 3
+		*/
+		
+		int n; // Nombre de lignes du probleme
+		int m; // Nombre de colonnes du probleme
+		bool problem[50][50];  // true => obstacle ; false => libre
+		
+		
+
+		void createGraph(); // Parcourt le probleme pour creer le graphe (sommet et liste d'adjacence pour les aretes)
+		void bfs();
+
+		void readProblem(); // Lit un probleme dans le fichier d'entree et renseigne n m problem xStart yStart xGoal yGoal dirStart
+		void writeSolution(); // Ecrit la solution dans le fichier resultat
 		
 };
 
